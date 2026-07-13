a1af2f06872347d7f716fd17cdc7a80fd0c23bc6
Szabo Istvan <pisti4395@gmail.com>
2026-07-13 11:56:37 +0200

    working

--- lua/nixvim_binary_runner/init.lua
+++ lua/nixvim_binary_runner/init.lua
@@ -11,29 +11,29 @@ end
 function M.test()
   print("hello from the plugin")
 end
---
---local function create_repl(command, name)
---    return function()
---        -- Create new split
---        --vim.api.nvim_command("vsplit")
---        vim.api.nvim_command("tabnew")
---        local buf = vim.api.nvim_create_buf(false, true)
---        vim.api.nvim_command("buffer " .. buf)
---        
---        -- Start REPL
---        vim.fn.termopen(command, {
---            on_exit = function(_, code)
---                print(name .. " REPL exited with code: " .. code)
---            end
---        })
---        
---        -- Enter insert mode to start typing
---        vim.cmd("startinsert")
---    end
---end
+
+local function create_repl(command, name)
+    return function()
+        -- Create new split
+        --vim.api.nvim_command("vsplit")
+        vim.api.nvim_command("tabnew")
+        local buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_command("buffer " .. buf)
+        
+        -- Start REPL
+        vim.fn.termopen(command, {
+            on_exit = function(_, code)
+                print(name .. " REPL exited with code: " .. code)
+            end
+        })
+        
+        -- Enter insert mode to start typing
+        vim.cmd("startinsert")
+    end
+end
 --
 ---- Register REPL commands
---vim.api.nvim_create_user_command('Steel', create_repl("steel", "Steel"), {})
---vim.api.nvim_create_user_command('Raa', create_repl("rust-ai-assistant", "ai"), {})
+vim.api.nvim_create_user_command('Steel', create_repl("steel", "Steel"), {})
+vim.api.nvim_create_user_command('Raa', create_repl("rust-ai-assistant", "ai"), {})
 
 return M
